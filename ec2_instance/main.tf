338f5d9a4d60c2185ccb58317a04ea2397e44be6
NallaballiGnaneswar <127484845+NallaballiGnaneswar@users.noreply.github.com>
2026-06-03 12:57:25 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "new_ubuntu.pem" 
+  key_name      = "new_ubuntu" 
   tags = {
     Name = var.instance_name
   }
