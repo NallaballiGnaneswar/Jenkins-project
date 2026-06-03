082fbb21954dd2b4c9521341f80c53bf54f1169e
NallaballiGnaneswar <ngnaneswar949194@gmail.com>
2026-06-03 12:17:09 +0530

    modified the variables.

--- Super-Project/variables.tf
+++ Super-Project/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
+  default = "ap-south-1"
 }
 
 variable "aws_type" {
   description = "AWS ec2 type"
-  default = "t3.medium"
+  default = "t3.small"
 }
 
 variable "aws_ami" {
   description = "AWS ami"
-  default = "ami-022661f8a4a1b91cf"
+  default = "ami-07a00cf47dbbc844c"
 }
